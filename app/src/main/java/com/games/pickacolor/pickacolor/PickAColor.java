d17d7feab8c5114f5508038d2979155c194c4ccd
BlackadderAnders <dslitefix@gmail.com>
2018-06-20 10:11:04 +0200

    first commit

---
+++ app/src/main/java/com/games/pickacolor/pickacolor/PickAColor.java
@@ -0,0 +1,11 @@
+package com.games.pickacolor.pickacolor;
+
+public class PickAColor {
+
+    private int WHITE = android.R.color.white;
+    private int YELLOW = android.R.color.black;
+    private int RED = android.R.color.holo_red_light;
+    private int BLUE = android.R.color.holo_blue_dark;
+    private int GREEN = android.R.color.holo_green_dark;
+    
+}
